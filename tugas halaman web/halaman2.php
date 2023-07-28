37a3c64c48e71e1104f7b41138b2ccc7b1aaf984
adin <111264781+adindd@users.noreply.github.com>
2023-07-28 20:55:12 +0700

    Add files via upload

---
+++ tugas halaman web/halaman2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>website</title>
+</head>
+<body>
+    <p><b><ins><big><big><big>Kucing besar</big></big></big></ins></b></p>
+        <div>
+        <img src="page2.jpg" alt="" width="25%">
+    </div>
+    <div class="container">
+    <br>
+    <br>
+    <div>
+        <p>Istilah <b>kucing besar</b> - yang bukan merupakan klasifikasi biologis - digunakan untuk membedakan spesies kucing yang lebih besar dari yang lebih kecil. Salah satu definisi "kucing besar" termasuk empat anggota genus Panthera: <a href="">harimau, singa, jaguar,</a> dan <a href="">macan tutul.</a> Anggota dari genus ini adalah satu-satunya kucing bisa mengaum. Definisi yang lebih luas dari "kucing besar" juga mencakup <a href="">cheetah, macan tutul salju,</a> dan <a href="">puma.</a></p>
+
+        <p> Meskipun perbedaan besar dalam ukuran, berbagai jenis kucing yang sangat mirip dalam struktur dan perilaku baik, dengan pengecualian cheetah, yang secara signifikan berbeda dari semua kucing yang besar ataupun kecil. Mereka bisa ditemukan di Amerika, Afrika, Asia, dan Eropa.</p>
+</body>
+</html>
---
+++ tugas halaman web/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>website</title>
+</head>
+<body>
+    <p><b><ins><big><big><big>Kucing</big></big></big></ins></b></p>
+    <div>
+    <img src="page1.jpg" alt="" width="25%">
+</div>
+<div class="container">
+<br>
+<br>
+<div>
+    <p>
+    <b>Kucing</b> disebut juga <b>kucing domestik</b> 
+    <sup><a href="">[4][5]</a></sup>
+    atau <b>kucing rumah</b>  
+    <a href="">(nama ilmiah:</a> 
+    <i><b>Felis silvestris catus atau Felis catus</b></i>) adalah sejenis <a href="">mamalia karnivora</a> dari <a href="">keluarga Felidae.</a> Kata "kucing" biasanya merujuk kepada "kucing" yang telah dijinakkan,<sup><a href="">[6]</a></sup> tetapi bisa juga bisa merujuk kepada 
+    <a href="">"kucing besar"</a> seperti <a href="">singa</a> dan <a href="">harimau</a> yang juga termasuk jenis kucing.
+    </p>
+
+    <p>
+    Kucing telah berbaur dengan kehidupan 
+    <a href="">manusia</a> paling tidak sejak 5.000 tahun SM, dengan ditemukannya <a href="">kerangka</a> kucing di <a href="">Pulau Siprus.</a>
+    <sup><a href="">[7]</a></sup>
+    Sejak zaman 3.500 SM, orang <a href="">Mesir kuno</a> telah menggunakan kucing untuk menjauhkan <a href="">tikus</a> atau <a href="">hewan pengerat</a> lain dari <a href="">lumbung</a> yang menyimpan hasil panen. <sup><a href="">[8]</a></sup>
+    </p>
+
+    <p>
+    Saat ini, kucing adalah salah satu <a href="">hewan peliharaan</a> terpopuler di dunia.
+    <sup><a href="">[9]</a></sup>
+    Kucing yang garis keturunannya tercatat secara resmi sebagai kucing <a href="">trah</a>atau <a href="">galur murni</a> <b><i>(pure breed)</i></b>, seperti <a href="">persia, siam, manx, dan sphinx.</a> Kucing seperti ini biasanya dibiakkan di tempat pemeliharaan hewan resmi. Jumlah kucing ras hanyalah 1% dari seluruh kucing di dunia, sisanya adalah kucing dengan keturunan campuran, seperti <a href="">kucing liar</a> atau kucing kampung. Kucing juga merupakan jenis hewan peliharaan yang banyak diminati karena tingkah lakunya yang lucu.
+    </p>
+
+   
+</head>
+<body>
+     <a href="halaman2.php">Next</a>
+</body>
+</html>
+
